57d9d7e9dfd4638818245a75519fa5f8d33ba2c1
umrgrs <qxrvynilp@gmail.com>
2025-06-17 17:26:14 -0600

    Added sections components to home

--- src/app/home/sections-card/sections-card.ts
+++ src/app/home/sections-card/sections-card.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameData } from '../../interfaces/game-data';
+
 
 @Component({
   selector: 'app-sections-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sections-card.html',
   styleUrl: './sections-card.css'
 })
 export class SectionsCard {
 
+@Input({required:true}) game_data:GameData | undefined;
+
 }
+
+
--- src/app/home/sections/sections.ts
+++ src/app/home/sections/sections.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { SectionsCard } from "../sections-card/sections-card";
+import { GameData } from '../../interfaces/game-data';
 
 @Component({
   selector: 'app-sections',
-  imports: [],
+  imports: [SectionsCard],
   templateUrl: './sections.html',
   styleUrl: './sections.css'
 })
 export class Sections {
+best_selling_games: GameData[] = [
+    {
+      id: 1,
+      name: 'Assassin\'s Creed Valhalla',
+      price: 59.99,
+      image: 'image_1.jpg',
+      description: 'No description'
+    },
+    {
+      id: 2,
+      name: 'Cyberpunk 2077',
+      price: 49.99,
+      image: 'image_2.jpg',
+      description: 'No description'
+    },
+    {
+      id: 3,
+      name: 'FIFA 24',
+      price: 69.99,
+      image: 'image_3.jpg',
+      description: 'No description'
+    }
+  ];
 
+new_games: GameData[] = [
+    {
+      id: 1,
+      name: 'Dune: Awakening',
+      price: 39.99,
+      image: 'image_1.jpg',
+      description: 'No description'
+    },
+    {
+      id: 2,
+      name: 'Shadowverse: Worlds Beyond',
+      price: 10.99,
+      image: 'image_2.jpg',
+      description: 'No description'
+    },
+    {
+      id: 3,
+      name: 'Soulstone Survivors',
+      price: 7.99,
+      image: 'image_3.jpg',
+      description: 'No description'
+    }
+  ];
 }
