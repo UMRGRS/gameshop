cd1e8d4a89e0e215702f89243b629f0baeab899e
umrgrs <qxrvynilp@gmail.com>
2025-06-17 16:38:36 -0600

    Added home sections template

--- src/app/home/home/home.ts
+++ src/app/home/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeCarrousel } from "../home-carrousel/home-carrousel";
+import { Sections } from "../sections/sections";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeCarrousel],
+  imports: [HomeCarrousel, Sections],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
---
+++ src/app/home/sections-card/sections-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sections-card',
+  imports: [],
+  templateUrl: './sections-card.html',
+  styleUrl: './sections-card.css'
+})
+export class SectionsCard {
+
+}
