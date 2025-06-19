ba0e543e53131b2aa0b20e99f6bbc5781afdac6f
umrgrs <qxrvynilp@gmail.com>
2025-06-18 18:24:27 -0600

    Added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./global/header/header";
+import { Footer } from "./global/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
