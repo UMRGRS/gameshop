b18d73db7d7a9c85300d9834ca95ecefcc1671a4
umrgrs <qxrvynilp@gmail.com>
2025-08-04 11:59:06 -0600

    Added auth by user kind

--- src/app/global/inside-header/inside-header.ts
+++ src/app/global/inside-header/inside-header.ts
@@ -1,28 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 import { CartService } from '../../services/cart-service';
+import { SessionManagement } from '../../services/session-management';
 
 @Component({
   selector: 'app-inside-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inside-header.html',
   styleUrl: './inside-header.css'
 })
-export class InsideHeader {
-  constructor(private auth_service:AuthService , private router: Router, private cart_service:CartService){}
-  
-    logout(){
-      this.cart_service.removeCart();
-      this.auth_service.logout();
-      this.router.navigate([''])
-    }
-    
-    toLibrary(){
-      this.router.navigate(['library'])
-    }
+export class InsideHeader implements OnInit{
+  constructor(private auth_service:AuthService , private router: Router, private cart_service:CartService,  private cd: ChangeDetectorRef, private session_management: SessionManagement){}
 
-    toCart(){
-    this.router.navigate(['cart'])
-    }
+  isAdmin:boolean = false;
+  isModerator:boolean = false;
+  
+  logout(){
+    this.cart_service.removeCart();
+    this.auth_service.logout();
+    this.router.navigate([''])
+  }
+
+  ngOnInit() {
+    this.isAdmin = this.session_management.isAuthenticatedAndIsAdmin();
+    this.isModerator = this.session_management.isAuthenticatedAndIsMod();
+    this.cd.detectChanges();
+  }
 }
